47c5c0a18e0d382cbc04c9a0f7f6a86fd37ebb98
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2024-03-22 16:59:49 +0100

    Cors Error Handling

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@
 import express from "express"
 import expressWs from "express-ws"
 import expresssession from "express-session" 
-
+import cors from "cors"
 
 //algo()
 // speichere gemischte Angebotsnehmer in die Datenbank
@@ -18,7 +18,7 @@ let datenbank = {
     "LobbyID":["1"],
     "spielID":[1, 2],
     "wirt":["1"],
-    "spieler":["1, 2"],
+    "spieler":[["1, 2"]],
     "lobby_kennwort":["00000"],
     "name": ["I3a"]
   },
@@ -48,10 +48,11 @@ let datenbank = {
   }
 }
 export async function startExpress() {
-  var session = expresssession()
   var app = express();
+  var session = expresssession();
   expressWs(app);
   //Registrierungs Magie   /register
+  app.use(cors()); 
   
 
   //Login magie   /login
@@ -73,6 +74,7 @@ export async function startExpress() {
   //jedes mal wenn wir vom Frontend eine Anfrage für eine Neue Lobby erhalten, /lobby/create   -----
   app.post("/lobby/create", (req, res) => {
     
+
     //wird kontrolliert ob der Nutzer bereits eine lobby offen hat:
     //hole alle nutzer
     
@@ -85,7 +87,8 @@ export async function startExpress() {
     let newCode = Math.floor(Math.random() * 90000) + 10000
     //und in der Datenbank gespeichert
 
-    
+    datenbank.Lobby.lobby_kennwort.push(newCode)
+    console.log(JSON.stringify(newCode))
     //sende den generierten lobbycode an das frontend
     res.send(JSON.stringify(newCode))
   })
@@ -99,40 +102,28 @@ export async function startExpress() {
   // ist der Code vorhanden:
     // hohle daten aus der datenbank mit dem Lobbycode
     // lade es zum nutzer so
-    
-    var lobbies = app.get("/lobby/create", (request, response) =>{
-      const lobbies = request.query;
-      console.log(lobbies);
-      return lobbies
-  })
-  var num = ["00000", "99999", "93845", "99303"]
+    app.ws('/', function(ws, req) {
+      ws.on('message', function(msg) {
+        console.log(msg);
+        
+        ws.send("AAAAA")
+      });
+      console.log('socket', req.testing);
+      
+    });
+  var lobbycode = "00000"
   // Das ist ein Websocket/Lobbie   /lobby/:00000   -----
-  app.ws('/lobby/', function(ws, req) {
+  app.ws('/lobby/00000', function(ws,req) {
     ws.on('message', function(msg) {
       console.log(msg);
     });
-    ws.send("OOOOOOOOOO")
-    
-    if (req.includes(lobbies)){
-      //sag backend er soll Nutzer umleiten
+    console.log('socket', req.testing);      
+  })
 
-    }
-    
-
-  });
   
   
   // Speichere den Code der Anfrage in eine Variabel  
-  
-  app.ws('/lobby/'+num, function(ws, req) {
-    ws.on('message', function(msg) {
-      console.log(msg);
-    });
-    console.log('socket AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', req.testing);
-    ws.send("AAAAAAAAAAAAAAAAAAAAAA")
-  });
-  
-  // wenn Nutzer nicht eingeloggt:
+    // wenn Nutzer nicht eingeloggt:
       // trage Schüler in die Datenbank ein
       // schicke anzahl Spieler in Lobby an den Lehrer
     // else
@@ -189,6 +180,7 @@ export async function startExpress() {
   
   return app.listen(8080);
 }
+
 startExpress()
 
 
