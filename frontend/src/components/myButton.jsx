b387545399e8920fd0f1eb974cf3bea9932cd570
ArcadeFortune <alessionights@gmail.com>
2024-04-01 18:56:52 +0200

    dev: myButton can optionally fill all available room

--- frontend/src/components/myButton.jsx
+++ frontend/src/components/myButton.jsx
@@ -1,21 +1,45 @@
-import Button from '@mui/joy/Button';
+import Button from "@mui/joy/Button";
 
-export default function MyButton({onClick, sx, children}) {
+export default function MyButton({
+  shouldFillAll = false,
+  onClick,
+  sx,
+  children,
+}) {
   let defaultStyle = {
-    width: "60%", height: "2em", fontSize: '2.5em', borderRadius: 12, fontWeight: "100", ...sx
-  }
+    width: shouldFillAll ? "100%" : "60%",
+    height: shouldFillAll ? "100%" : "2em",
+    fontSize: "2.5em",
+    borderRadius: 12,
+    fontWeight: "100",
+    ...sx,
+  };
+
   return (
     <div
-    style={{
-      position: "relative",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      textAlign: "center",
-      marginTop: "1.3em",
-    }}
-    > 
-      <Button onClick={onClick} sx={defaultStyle} color='neutral' variant='solid'>{children}</Button>
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        width: shouldFillAll ? "100%" : "auto",
+        height: shouldFillAll ? "100%" : "auto",
+      }}
+    >
+      <Button
+        onClick={onClick}
+        sx={defaultStyle}
+        color="neutral"
+        variant="solid"
+        slotProps={{
+          button: {
+            sx: {
+              width: shouldFillAll ? "100%" : "auto",
+              height: shouldFillAll ? "100%" : "auto",
+            },
+          },
+        }}
+      >
+        {children}
+      </Button>
     </div>
-  )
+  );
 }
