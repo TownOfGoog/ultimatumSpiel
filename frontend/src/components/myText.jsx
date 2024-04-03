74b75cf53476fbfa364dfbccbf352395ce1a098e
ArcadeFortune <alessionights@gmail.com>
2024-04-03 23:56:44 +0200

    fix: use correct fontzise when waiting for players in host pov

--- frontend/src/components/myText.jsx
+++ frontend/src/components/myText.jsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/joy";
 
 export default function MyText({isInNav, title, bold, color, small, ...props}) {
-  const sx = { color: color ? color : isInNav ? '' : 'black', fontWeight: bold ? 'bold' : 'normal', fontSize: title ? '3.5em' : 'auto', ...props.sx };
+  const sx = { color: color ? color : isInNav ? '' : 'black', fontWeight: bold ? 'bold' : 'normal', fontSize: title ? '3.5em' : props.fontSize, ...props.sx };
 
   return(
     <Typography level="h1" {...props} sx={sx}>
