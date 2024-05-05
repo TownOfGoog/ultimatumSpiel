1d8c01f374a09ec12c81cf7a35ac301b53edde9e
Alessio <46051524+ArcadeFortune@users.noreply.github.com>
2024-05-05 17:27:25 +0200

    undo wrong commit

--- Backend/lobby_tester.js
+++ Backend/lobby_tester.js
@@ -133,7 +133,7 @@ export class Lobby_tester {
               });
               if (!error) { this.players[i].done() }
               //to give time for the server to handle everything properly
-            }, 2000); 
+            }, 20); 
           },
           done: () => {},
         };
